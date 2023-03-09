b007cd003eddc78d525d76105db800aeee744b65
davidmiles <kingdavidmiles@gmail.com>
2023-03-09 13:04:20 +0100

    first commit

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import axios from 'axios'
+import VueSweetalert2 from 'vue-sweetalert2';
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
+Vue.use( axios)
+Vue.use(VueSweetalert2);
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 new Vue({
   render: h => h(App),
